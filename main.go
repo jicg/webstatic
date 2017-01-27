ccb1c28d246f9c7455589d11dc52a8d9a843b9ef
jicg <284077319@qq.com>
2017-01-27 17:16:16 +0800

    初始化

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"gopkg.in/macaron.v1"
+)
+
+func main() {
+	m := macaron.New()
+	dir := "static"
+	m.Use(macaron.Static(dir, macaron.StaticOptions{
+		Prefix:      "/",
+		SkipLogging: true,
+	}))
+	m.Run(8001)
+}
